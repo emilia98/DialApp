1c1e8178ad0fd5d4bbcf22ee4b5922a4f48ac630
Emilia Nedyalkova <emilia_nedialkova@abv.bg>
2021-04-26 10:38:44 +0300

    refactor some code for styling buttons

--- CallApp/CallApp/ViewController.swift
+++ CallApp/CallApp/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController {
     @IBOutlet var buttonSix: UIButton!
     @IBOutlet var buttonSeven: UIButton!
     @IBOutlet var buttonEight: UIButton!
+    @IBOutlet var numberLabel: UILabel!
+    @IBOutlet var addNumberButton: UIButton!
     
     private var buttons: [ String: (attributed: Bool, mainText: String?, secondaryText: String?, target: UIButton)] = [:]
     
@@ -44,28 +46,16 @@ class ViewController: UIViewController {
         ]
         
         callButton.imageView?.contentMode = .scaleAspectFit
-        callButton.clipsToBounds = true
-        callButton.layer.cornerRadius = callButton.bounds.size.width * 0.5
-
-        
-        
-        // callButton.se
-        // buttonZero.titleLabel?.numberOfLines = 0
-        
-        formatAttributedString("0")
-        formatAttributedString("1")
-        formatAttributedString("2", 12, -5)
-        formatAttributedString("3", 12, -5)
-        formatAttributedString("4", 12, -5)
-        formatAttributedString("5", 12, -5)
-        formatAttributedString("6", 12, -5)
-        formatAttributedString("7", 12, -5)
-        formatAttributedString("8", 12, -5)
-        formatAttributedString("9", 12, -5)
         
         for key in buttons.keys {
             let button = buttons[key]!.target
             shapeButton(button)
+            
+            if key == "0" || key == "1" {
+                formatAttributedString(key, 18, -10)
+            } else {
+                formatAttributedString(key)
+            }
         }
     }
     
@@ -74,7 +64,7 @@ class ViewController: UIViewController {
         button.layer.cornerRadius = callButton.bounds.size.width * 0.5
     }
     
-    private func formatAttributedString(_ key: String, _ fontSecondary: CGFloat = 18, _ spacing: CGFloat = -10) {
+    private func formatAttributedString(_ key: String, _ fontSecondary: CGFloat = 12, _ spacing: CGFloat = -5) {
         if !buttons[key]!.attributed {
             return
         }
@@ -82,13 +72,11 @@ class ViewController: UIViewController {
         let buttonSettings = buttons[key]!
         let button = buttonSettings.target
         button.titleLabel?.textAlignment = .center
-        // button.titleLabel?.lineBreakMode = .byWordWrapping
+        button.titleLabel?.lineBreakMode = .byWordWrapping
         button.titleLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping;
-        // let buttonText = "\(button.main)\n\()"
         
         let paragraph = NSMutableParagraphStyle()
         paragraph.alignment = .center
-        
         
         let fontMain = UIFont.systemFont(ofSize: 36)
         let attributesMain = [NSMutableAttributedString.Key.font: fontMain, .paragraphStyle: paragraph]
@@ -102,59 +90,4 @@ class ViewController: UIViewController {
         attributedTextMain.append(attributedTextSecondary)
         button.setAttributedTitle(attributedTextMain, for: [])
     }
-    
-    
-    private func test(_ key: String) {
-        if !buttons[key]!.attributed {
-            return
-        }
-        
-        let buttonSettings = buttons[key]!
-        let button = buttonSettings.target
-        
-        button.titleLabel?.textAlignment = .center
-        button.titleLabel?.numberOfLines = 0
-        /*
-        let rect = CGRect(x: 0, y: 0, width: button.layer.bounds.width,
-                          height: button.layer.bounds.height)
-        button.frame = rect */
-        //button.titleLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping;
-        let buttonText: NSString = "0\n+"
-        let paragraph = NSMutableParagraphStyle()
-        paragraph.alignment = .center
-       // para
-        
-          //getting the range to separate the button title strings
-          let newlineRange: NSRange = buttonText.range(of: "\n")
-
-          //getting both substrings
-          var substring1 = ""
-          var substring2 = ""
-
-          if(newlineRange.location != NSNotFound) {
-              substring1 = buttonText.substring(to: newlineRange.location)
-              substring2 = buttonText.substring(from: newlineRange.location)
-          }
-
-          //assigning diffrent fonts to both substrings
-        let font1: UIFont = UIFont.systemFont(ofSize: 39)// (name: "Arial", size: 44)!
-        print(substring1, substring2)
-          let attributes1 = [NSMutableAttributedString.Key.font: font1, .paragraphStyle: paragraph]
-          let attrString1 = NSMutableAttributedString(string: substring1, attributes: attributes1)
-
-        paragraph.paragraphSpacingBefore = -8
-        let font2: UIFont = UIFont.systemFont(ofSize: 15, weight: .bold)// (ofSize: 15) // (name: "Arial", size: 18)!
-          let attributes2 = [NSMutableAttributedString.Key.font: font2, .paragraphStyle: paragraph]
-          let attrString2 = NSMutableAttributedString(string: substring2, attributes: attributes2)
-
-          //appending both attributed strings
-          attrString1.append(attrString2)
-        
-        print(attrString1)
-
-          //assigning the resultant attributed strings to the button
-        button.setAttributedTitle(attrString1, for: [])
-    }
-
-
 }
